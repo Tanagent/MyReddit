9f6b6af9e00992456d0b81f2ec765fce42a02990
Brian Tan <briantanw@gmail.com>
2016-02-10 01:03:57 -0800

    implemented JSON parsing

--- app/src/main/java/com/example/brian/myreddit/MainActivity.java
+++ app/src/main/java/com/example/brian/myreddit/MainActivity.java
@@ -1,21 +1,118 @@
 package com.example.brian.myreddit;
 
 import android.app.Activity;
+import android.app.ProgressDialog;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.VolleyLog;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class MainActivity extends Activity {
+
+    public static final String TAG = "MyRecyclerList";
+    private List<ListItems> listItemsList = new ArrayList<ListItems>();
+
+    private RecyclerView mRecyclerView;
+    private MyRecyclerAdapter adapter;
+
+    private int counter = 0;
+    private String count;
+    private String jsonSubreddit;
+    private String after_id;
+    private static final String gaming = "gaming";
+    private static final String aww = "aww";
+    private static final String funny = "funny";
+    private static final String food = "food";
+    private static final String subredditUrl = "http://www.reddit.com/r/";
+    private static final String jsonEnd = "/.json";
+    private static final String qCount = "?count=";
+    private static final String after = "&after=";
+
+    private ProgressDialog progressDialog;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mRecyclerView = (RecyclerView) findViewById(R.id.recycler_view);
+
+        final LinearLayoutManager linearLayoutManager = new LinearLayoutManager(this);
+        mRecyclerView.setLayoutManager(linearLayoutManager);
+    }
+
+    public void updateList(String subreddit) {
+        counter = 0;
+
+        subreddit = subredditUrl + subreddit + jsonEnd;
+
+        adapter = new MyRecyclerAdapter(MainActivity.this, listItemsList);
+        mRecyclerView.setAdapter(adapter);
+
+        RequestQueue queue = Volley.newRequestQueue(this);
+
+        adapter.clearAdapter();
+
+        JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(Request.Method.GET, subreddit, null, new Response.Listener<JSONObject>() {
+            @Override
+            public void onResponse(JSONObject jsonObject) {
+                Log.d(TAG, jsonObject.toString());
+
+                try {
+                    JSONObject data = jsonObject.getJSONObject("data");
+                    after_id = data.getString("after");
+                    JSONArray children = data.getJSONArray("children");
+
+                    for (int i = 0; i < children.length(); i++) {
+                        JSONObject post = children.getJSONObject(i).getJSONObject("data");
+
+                        ListItems item = new ListItems();
+
+                        item.setTitle(post.getString("title"));
+                        item.setThumbnail(post.getString("thumbnail"));
+                        item.setUrl(post.getString("url"));
+                        item.setSubreddit(post.getString("subreddit"));
+                        item.setAuthor(post.getString("author"));
+
+                        jsonSubreddit = post.getString("subreddit");
+
+                        listItemsList.add(item);
+                    }
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+                adapter.notifyDataSetChanged();
+            }
+        }, new Response.ErrorListener() {
+                @Override
+                public void onErrorResponse(VolleyError error) {
+                    VolleyLog.d(TAG, "Error" + error.getMessage());
+                }
+        });
+
+        queue.add(jsonObjectRequest);
     }
 
     @Override
